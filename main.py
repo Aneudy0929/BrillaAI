0c772fc197d8881d730746f1bda5f52ef798308a
Aneudy0929 <aneudyullola2002@gmail.com>
2026-07-07 18:50:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # con el valor de tu clave secreta.
 cliente_groq = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
-    
+    base_url="https://api.groq.com/openai/v1"
 )
 
 class Consulta(BaseModel):
